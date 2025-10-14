c04eae0b5e6e6892f493dec97743e9a880ca8db4
kavya478 <kummithakavya@gmail.com>
2025-10-14 08:01:35 +0530

    add

---
+++ 60-acm/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
+
+
+backend "s3" {
+    bucket = "divya-tf-remote-states"
+    key    = "tsools-cicd" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+    region = "us-east-1"
+    dynamodb_table = "kavyas-tf-remote-state-dev"
+  }
+}
+
+
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
